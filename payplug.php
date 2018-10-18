ba5066cc678ae1a6db0c16f3ef9f64067c4654d1
Clement Boirie <cboirie@beapi.fr>
2018-10-18 16:12:51 +0200

    avoid PHP notice if WooCommerce is not activated

--- payplug.php
+++ payplug.php
@@ -32,10 +32,6 @@ function init() {
 		require_once plugin_dir_path( __FILE__ ) . '/vendor/autoload.php';
 	}
 
-	if ( PayplugWoocommerceHelper::is_pre_30() ) {
-		require_once plugin_dir_path( __FILE__ ) . '/woocommerce-compat.php';
-	}
-
 	load_plugin_textdomain( 'payplug', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
 
 	PayplugWoocommerce::get_instance();
