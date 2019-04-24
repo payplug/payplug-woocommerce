88a861d7df4c74b3d5f9927872738fa56bbb8bfd
Clement Boirie <cboirie@beapi.fr>
2019-04-24 17:36:53 +0200

    add new ajax handler

---
+++ src/Admin/Ajax.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Payplug\PayplugWoocommerce\Admin;
+
+// Exit if accessed directly
+use Payplug\PayplugWoocommerce\Gateway\PayplugGateway;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * PayPlug admin ajax handler.
+ *
+ * @package Payplug\PayplugWoocommerce\Admin
+ */
+class Ajax {
+
+	const REFRESH_KEY_ACTION = 'payplug_refresh_keys';
+
+	public function __construct() {
+		add_action( 'wp_ajax_' . self::REFRESH_KEY_ACTION, [ $this, 'handle_refresh_keys' ] );
+	}
+
+	public function handle_refresh_keys() {
+
+		if ( empty( $_POST['_wpnonce'] ) || empty( $_POST['email'] ) || empty( $_POST['password'] ) ) {
+			wp_send_json_error(
+				array(
+					'message' => __( 'Invalid request.', 'payplug' ),
+				)
+			);
+		}
+
+		$action = sprintf( '%s_%s', wp_unslash( $_POST['email'] ), self::REFRESH_KEY_ACTION );
+		if ( ! check_ajax_referer( $action ) ) {
+			wp_send_json_error(
+				array(
+					'message' => __( 'Invalid nonce.', 'payplug' ),
+				)
+			);
+		}
+
+		$email    = sanitize_text_field( wp_unslash( $_POST['email'] ) );
+		$password = sanitize_text_field( wp_unslash( $_POST['password'] ) );
+
+		if ( ! WC()->payment_gateways() ) {
+			wp_send_json_error(
+				array(
+					'message' => __( 'An error occured with PayPlug gateway. Please make sure PayPlug settings are correct.', 'payplug' ),
+				)
+			);
+		}
+
+		$payment_gateways = WC()->payment_gateways()->payment_gateways();
+		if ( empty( $payment_gateways ) || ! isset( $payment_gateways['payplug'] ) ) {
+			wp_send_json_error(
+				array(
+					'message' => __( 'An error occured with PayPlug gateway. Please make sure PayPlug settings are correct.', 'payplug' ),
+				)
+			);
+		}
+
+		/* @var PayplugGateway $payplug_gateway */
+		$payplug_gateway = $payment_gateways['payplug'];
+		$keys            = $payplug_gateway->retrieve_user_api_keys( $email, $password );
+		if ( is_wp_error( $keys ) ) {
+			wp_send_json_error(
+				array(
+					'message' => $keys->get_error_message(),
+				)
+			);
+		}
+
+		$this->update_api_keys( $keys, $payplug_gateway );
+
+		wp_send_json_success(
+			array(
+				'message' => __( 'Your API keys has successfully been updated.', 'payplug' ),
+			)
+		);
+	}
+
+	/**
+	 * Update PayPlug api keys
+	 *
+	 * @param array $keys
+	 * @param PayplugGateway $payplug_gateway
+	 *
+	 * @return bool
+	 */
+	protected function update_api_keys( $keys, $payplug_gateway ) {
+		if ( empty( $payplug_gateway->settings ) ) {
+			$payplug_gateway->init_settings();
+		}
+
+		$payplug_gateway->settings['payplug_test_key'] = $keys['test'];
+		$payplug_gateway->settings['payplug_live_key'] = $keys['live'];
+
+		return update_option(
+			$payplug_gateway->get_option_key(),
+			apply_filters(
+				'woocommerce_settings_api_sanitized_fields_' . $payplug_gateway->id,
+				$payplug_gateway->settings
+			),
+			'yes'
+		);
+	}
+}
