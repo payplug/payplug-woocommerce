b66e35026fb1b34ae2039702b3aba66395509c18
Thomas Pinvin <pinvint@quadra-informatique.fr>
2020-09-21 15:15:57 +0200

    WOOC-109-- Check if clear persmissions

--- src/Gateway/PayplugGateway.php
+++ src/Gateway/PayplugGateway.php
@@ -613,7 +613,9 @@ class PayplugGateway extends WC_Payment_Gateway_CC {
 			&& false !== check_admin_referer( 'payplug_user_logout', '_logoutaction' )
 		) {
 
-			$this->permissions->clear_permissions();
+			if($this->permissions) {
+				$this->permissions->clear_permissions();
+			}
 
 			$data                        = get_option( $this->get_option_key() );
 			$data['payplug_test_key']    = '';
