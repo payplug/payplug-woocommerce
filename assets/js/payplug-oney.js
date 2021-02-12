5b0e9288960bb92f1eb7c98c805b97cd443ddaeb
Thomas Pinvin <thomas.pinvin@quadra-informatique.fr>
2021-02-12 11:07:51 +0100

    WOOC-148 - Modif affichage popup page produit

--- assets/js/payplug-oney.js
+++ assets/js/payplug-oney.js
@@ -1,15 +1,18 @@
 (function ($) {
     var popupLoaded = false
-    var popupIsLoading = false
     var initevent = function () {
         var showpopup = $("#oney-show-popup")
         var showpopuponey = $("#oney-show-popup").closest('.payplug-oney')
-        var disabled = showpopuponey.hasClass('disabled');
+        var arrow = $("#oney-popup-arrow")
+        var qtyInput = $('input[name=quantity]')
         var popup = $("#oney-popup")
+        var loading = popup.find('.payplug-lds-roller')
+        var oneyError = popup.find('#oney-popup-error')
         var showpopupF = function () {
             popup.show(0, function () {
                 if (!$.browser.mobile) {
-					if (disabled) {
+                    checkOneyError()
+                    if (showpopuponey.hasClass('disabled')) {
                         var top = 50
                     } else {
                         var top = 110
@@ -20,54 +23,97 @@
                         at: popupLoaded ? "right+40 bottom" : "right bottom",
                         of: showpopup,
                     })
-                    showarrow()
+                    if(popupLoaded) {
+                        showarrow()
+                    }
                 }
             })
         }
+
         showarrow = function () {
-            var arrow = $("#oney-popup-arrow")
+            arrow.show()
             arrow.position({
-                my: "left top-30",
+                my: "left top-43",
                 at: "right+10 bottom",
                 of: showpopup,
             })
         }
 
+        calculTotals = function () {
+            var qty = qtyInput.val()
+            var price = showpopuponey.data('price')
+            return qty * price
+        }
+
+        isInOneyRange = function () {
+            var totalPrice = calculTotals()
+            var minOney = showpopuponey.data('min-oney')
+            var maxOney = showpopuponey.data('max-oney')
+            return (totalPrice >= minOney && totalPrice <= maxOney)
+        }
+
+        checkOneyError = function () {
+            if(showpopuponey.hasClass('disabled')) {
+                popupLoaded = true
+                popup.html('').append($(oneyError)).append($(arrow))
+                popup.addClass('loaded')
+                popup.find('.payplug-lds-roller').hide()
+                popup.find('#oney-popup-error .oney-error').hide()
+                qtyInput.val() >= 1000 ?
+                    popup.find('#oney-popup-error .oney-error.qty').show() :
+                    popup.find('#oney-popup-error .oney-error.range').show()
+            } else {
+                arrow.hide()
+            }
+        }
+
+        qtyInput.unbind()
+        qtyInput.on('change', function () {
+            popupLoaded = false
+            if (isInOneyRange()) {
+                showpopuponey.removeClass('disabled')
+                popup.removeClass('disabled').removeClass('loaded')
+                popup.html('').append($(arrow))
+                arrow.hide()
+            } else {
+                checkOneyError()
+                showpopuponey.addClass('disabled')
+                popup.addClass('disabled')
+            }
+        })
+
         showpopuponey.unbind()
         showpopuponey.on('click', function () {
-            showpopupF()
-            if (popupLoaded || popupIsLoading) {
-                return
-            }
-            var price = $(this).data('price')
-            popupIsLoading = true
-            $.post(
-                payplug_config.ajax_url,
-                {
-                    'action': payplug_config.ajax_action,
-                    'price': price
-                }, function (response) {
-                    popupIsLoading = false
-                    if (response.data.popup) {
-                        popupLoaded = true
-                        popup.addClass('loaded')
-                        popup.html(response.data.popup)
-                        showpopupF()
-                    }
+            if (isInOneyRange() && qtyInput.val() < 1000) {
+                if (popupLoaded) {
+                    return
                 }
-            )
+                popup.html('').append($(loading))
+                $.post(
+                    payplug_config.ajax_url,
+                    {
+                        'action': payplug_config.ajax_action,
+                        'price': calculTotals()
+                    }, function (response) {
+                        if (response.data.popup) {
+                            popupLoaded = true
+                            popup.addClass('loaded')
+                            popup.html(response.data.popup)
+                            showpopupF()
+                        }
+                    }
+                )
+            } else {
+                checkOneyError()
+            }
         })
 
 
         showpopuponey.on('mouseenter', function () {
-            if (popupLoaded) {
-                showpopupF()
-            }
+            showpopupF()
         })
         showpopuponey.on('mouseleave', function () {
-            if (popupLoaded) {
-                popup.hide()
-            }
+            popup.hide()
         })
 
         $(document).on('scroll', function () {
