6649b639dbd2381105ba103fc638e2715830d401
Thomas Pinvin <pinvint@quadra-informatique.fr>
2021-10-28 15:06:20 +0200

    WOOC-256 : Bump version

--- payplug.php
+++ payplug.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.payplug.com/
  * Text Domain:     payplug
  * Domain Path:     /languages
- * Version:         1.2.11.2
+ * Version:         1.2.11.3
  * WC tested up to: 5.3.0
  * License:         GPLv3 or later
  * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
