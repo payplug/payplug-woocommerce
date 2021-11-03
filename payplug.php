da9b7b19a531809a36e4dda5dda1a9c4ca1ad36e
Thomas Pinvin <pinvint@quadra-informatique.fr>
2021-11-03 15:55:28 +0100

    WOOC-256 : Bump version

--- payplug.php
+++ payplug.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.payplug.com/
  * Text Domain:     payplug
  * Domain Path:     /languages
- * Version:         1.3.0
+ * Version:         1.3.1
  * WC tested up to: 5.3.0
  * License:         GPLv3 or later
  * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'PAYPLUG_GATEWAY_VERSION', '1.3.0' );
+define( 'PAYPLUG_GATEWAY_VERSION', '1.3.1' );
 define( 'PAYPLUG_GATEWAY_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'PAYPLUG_GATEWAY_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'PAYPLUG_GATEWAY_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
