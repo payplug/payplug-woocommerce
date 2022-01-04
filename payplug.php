209809ef89e9f6beaabd5b05bc9ee972f7ddde7f
PPmmesquita <mmesquita-ext@payplug.com>
2022-01-04 12:50:03 +0000

    WOOC-234  Bump PayPlug version with a 4th digit

--- payplug.php
+++ payplug.php
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'PAYPLUG_GATEWAY_VERSION', '1.3.0' );
+define( 'PAYPLUG_GATEWAY_VERSION', '1.3.0.1' );
 define( 'PAYPLUG_GATEWAY_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'PAYPLUG_GATEWAY_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'PAYPLUG_GATEWAY_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
