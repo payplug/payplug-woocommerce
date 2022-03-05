7719612f6766223e3d8850bf42fc541092b70837
PPmmesquita <mmesquita-ext@payplug.com>
2022-03-05 03:03:56 +0000

    WOOC-284 phpunit + configs

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -8,12 +8,12 @@
 /**
  * Sample test case.
  */
-class SampleTest extends \PHPUnit\Framework\TestCase {
+class SampleTest extends WP_UnitTestCase {
 
 	/**
 	 * A single example test.
 	 */
-	function test_sample() {
+	public function test_sample() {
 		// Replace this with some actual testing code.
 		$this->assertTrue( true );
 	}
