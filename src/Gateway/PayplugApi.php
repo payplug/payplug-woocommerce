038b45ed9f30e1e6b7a75c6abb379f7aee46191d
Issam Hamed <ihamed-ext@payplug.com>
2022-03-28 17:24:34 +0100

    WOOC-342

--- src/Gateway/PayplugApi.php
+++ src/Gateway/PayplugApi.php
@@ -9,6 +9,8 @@ if (!defined('ABSPATH')) {
 
 use Payplug\Exception\BadRequestException;
 use Payplug\Exception\NotFoundException;
+use Payplug\Exception\PayplugServerException;
+use Payplug\Exception\UnauthorizedException;
 use Payplug\Payplug;
 use Payplug\Core\HttpClient;
 use Payplug\PayplugWoocommerce\PayplugWoocommerceHelper;
@@ -124,22 +126,24 @@ class PayplugApi {
         $country = wc_get_base_location();
         $oney_fees = ["x3_" . $oney_type, "x4_" . $oney_type];
 
-		try{
-			try {
-				$response =  $this->do_request('\Payplug\OneySimulation::getSimulations', [[
-					"amount" => (int) $price * 100,
-					"country" => $country['country'],
-					"operations" => $oney_fees
-				]]);
-
-
-				PayplugWoocommerceHelper::oney_simulation_values($oney_fees, $response);
-			} catch (\Payplug\Exception\PayplugServerException $e) {
-				$response = __('Your payment schedule simulation is temporarily unavailable. You will find this information at the payment stage.', 'payplug');
-			}
-		} catch( BadRequestException $e){
-			$response = __('Your payment schedule simulation is temporarily unavailable. You will find this information at the payment stage.', 'payplug');
-		}
+	    try {
+		    try {
+			    try {
+				    $response = $this->do_request('\Payplug\OneySimulation::getSimulations', [[
+					    "amount" => (int)$price * 100,
+					    "country" => $country['country'],
+					    "operations" => $oney_fees
+				    ]]);
+				    PayplugWoocommerceHelper::oney_simulation_values($oney_fees, $response);
+			    } catch (PayplugServerException $e) {
+				    $response = __('Your payment schedule simulation is temporarily unavailable. You will find this information at the payment stage.', 'payplug');
+			    }
+		    } catch (BadRequestException $e) {
+			    $response = __('Your payment schedule simulation is temporarily unavailable. You will find this information at the payment stage.', 'payplug');
+		    }
+	    } catch (UnauthorizedException $e) {
+		    $response = __('Your payment schedule simulation is temporarily unavailable. You will find this information at the payment stage.', 'payplug');
+	    }
 
         return $response;
     }
