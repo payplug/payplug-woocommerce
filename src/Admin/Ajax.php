4255e50ce60d4d6829688d01d51f1bb1fc239e6a
ihamed-ext@payplug.com <ihamed-ext@payplug.com>
2022-08-30 08:46:23 +0100

    WOOC-425

--- src/Admin/Ajax.php
+++ src/Admin/Ajax.php
@@ -36,6 +36,69 @@ class Ajax {
 		add_action( 'wp_ajax_' . self::CHECK_LIVE_PERMISSIONS, [ $this, 'check_live_permissions' ] );
 		add_action( 'wp_ajax_' . self::CHECK_BANCONTACT_PERMISSIONS, [ $this, 'check_bancontact_permissions' ] );
 		add_action( 'wp_ajax_' . self::CHECK_APPLEPAY_PERMISSIONS, [ $this, 'check_applepay_permissions' ] );
+		$authenticated = current_user_can('administrator');
+		add_action( 'rest_api_init', function () use($authenticated) {
+			register_rest_route( 'payplug', '/get_data', array(
+				'methods' => 'GET',
+				'callback' => array($this, "get_data"),
+				'permission_callback' => function() use($authenticated) {return $authenticated;}
+			) );
+		} );
+	}
+
+	public function get_data() {
+		$options = get_option('woocommerce_payplug_settings', []);
+		$options["user_logged_in"] = $this->user_logged_in($options);
+		$translations = [
+			"login" => [
+				"payplug_login_title" => __("payplug_login_title", "payplug"),
+				"payplug_login_description" => __("payplug_login_description", "payplug"),
+				"payplug_email_address" => __("payplug_email_address", "payplug"),
+				"payplug_password" => __("payplug_password", "payplug"),
+				"payplug_connect_button" => __("payplug_connect_button", "payplug"),
+				"payplug_credentials_error" => __("payplug_credentials_error", "payplug"),
+				"payplug_ok" => __("payplug_ok", "payplug"),
+				"payplug_not_registered_yet_button" => __("payplug_not_registered_yet_button", "payplug"),
+				"payplug_forget_password" => __("payplug_forget_password", "payplug"),
+			],
+			"register" => [
+				"payplug_register_title" => __("payplug_register_title", "payplug"),
+				"payplug_register_description" => __("payplug_register_description", "payplug"),
+				"payplug_register_note" => __("payplug_register_note", "payplug"),
+				"payplug_create_account_button" => __("payplug_create_account_button", "payplug"),
+				"payplug_create_account_button_link" => __("payplug_create_account_button_link", "payplug"),
+				"payplug_already_have_account_button" => __("payplug_already_have_account_button", "payplug"),
+			],
+			"general" => [
+				"payplug_general_title" => __("payplug_general_title", "payplug"),
+				"payplug_general_description" => __("payplug_general_description", "payplug"),
+				"payplug_logout" => __("payplug_logout", "payplug"),
+				"payplug_portal_link" => __("payplug_portal_link", "payplug"),
+				"payplug_mode" => __("payplug_mode", "payplug"),
+				"payplug_test_mode_note" => __("payplug_test_mode_note", "payplug"),
+				"payplug_live_mode_note" => __("payplug_live_mode_note", "payplug"),
+				"payplug_learn_more" => __("payplug_learn_more", "payplug"),
+				"payplug_learn_more_link" => __("payplug_learn_more_link", "payplug"),
+				"payplug_test" => __("payplug_test", "payplug"),
+				"payplug_live" => __("payplug_live", "payplug"),
+			],
+			"status" => [
+				"payplug_status_title" => __("payplug_status_title", "payplug"),
+				"payplug_status_description" => __("payplug_status_description", "payplug"),
+			]
+		];
+
+		return ["data" => $options, "translations" => $translations];
+	}
+
+	/**
+	 * Check if user is logged in and we have an API key for TEST mode.
+	 *
+	 * @return bool
+	 */
+	public function user_logged_in($options)
+	{
+		return !empty($options['payplug_test_key']);
 	}
 
 	public function handle_refresh_keys() {
@@ -125,7 +188,6 @@ class Ajax {
         $permissions = $account['httpResponse']['permissions'];
 		wp_send_json_success($permissions);
 	}
-
 
 	public function check_bancontact_permissions() {
 		try{
