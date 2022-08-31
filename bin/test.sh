536303d8e138e253a41ffcaf0543a1436467fb26
ihamed-ext@payplug.com <ihamed-ext@payplug.com>
2022-08-31 14:47:41 +0100

    WOOC-442

--- bin/test.sh
+++ bin/test.sh
@@ -7,5 +7,12 @@ set -e
 cd "$(dirname "$0")"
 cd ..
 
+# Enable nicer messaging for build status
+YELLOW_BOLD='\033[1;33m';
+COLOR_RESET='\033[0m';
+status () {
+	echo -e "\n${YELLOW_BOLD}$1${COLOR_RESET}\n"
+}
+
+status "ERROR: Cannot build plugin zip with dirty working tree."
 vendor/bin/phpcs --colors -s ./src/ --standard=PHPCompatibility --runtime-set testVersion 5.3
-echo "ERROR: Cannot build plugin zip with dirty working tree."
