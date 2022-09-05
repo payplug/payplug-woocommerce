ed5c1f7a68a5907cb0bdb25c38d35fd50521e28e
ihamed-ext <ihamed-ext@payplug.com>
2022-09-05 10:24:58 +0100

    WOOC-416

--- assets/js/payplug-admin-amex.js
+++ assets/js/payplug-admin-amex.js
@@ -10,10 +10,8 @@
 			this.check_american_express();
 			$('input[name=woocommerce_payplug_mode]').on('click', this.check_american_express); //mode
 
-
 		},
 		check_american_express_permissions: (callback) => {
-
 			payplug_admin.xhr = $
 				.post(
 					payplug_admin_config.ajax_url,
@@ -25,31 +23,28 @@
 		},
 		check_american_express: (event)=> {
 
+			payplug_admin.disableamerican_express();
+
 			if(payplug_admin.isTestMode()){
 				payplug_admin.uncheck_american_express();
-				payplug_admin.disable_american_express();
-				$("#amex_test_mode_description").show();
-				$("#amex_live_mode_description").hide();
+				payplug_admin.disableamerican_express();
 				return;
-			} else {
-				payplug_admin.enable_american_express();
-				$("#amex_test_mode_description").hide();
-				$("#amex_live_mode_description").show();
 			}
 
 			payplug_admin.check_american_express_permissions((res) => {
 				if(false === res.success){
 					payplug_admin.uncheck_american_express();
-					payplug_admin.disable_american_express();
+					payplug_admin.disableamerican_express();
 					return;
 				}
 
 				if(false === res.data){
 					payplug_admin.uncheck_american_express();
+					payplug_admin.disableamerican_express();
 					return;
 				}
 
-				payplug_admin.enable_american_express();
+				payplug_admin.enableamerican_express();
 			});
 
 		},
@@ -59,10 +54,10 @@
 		uncheck_american_express: function(){
 			jQuery("#woocommerce_payplug_american_express").prop("checked", false);
 		},
-		disable_american_express: function(){
+		disableamerican_express: function(){
 			jQuery("#woocommerce_payplug_american_express").prop("disabled", true);
 		},
-		enable_american_express: function(){
+		enableamerican_express: function(){
 			jQuery("#woocommerce_payplug_american_express").prop("disabled", false);
 		}
 	}
