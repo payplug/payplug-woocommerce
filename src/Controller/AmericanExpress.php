8acae88d690aef34aa0aa0192e926b6d073c48a0
Djalel <ghabri.djalel@gmail.com>
2022-09-19 17:23:57 +0100

    Amex payment and order update

--- src/Controller/AmericanExpress.php
+++ src/Controller/AmericanExpress.php
@@ -104,5 +104,97 @@ class AmericanExpress extends PayplugGateway
 	}
 	return $icons_str;
 }
+	/**
+	 * @param \WC_Order $order
+	 * @param int $amount
+	 * @param int $customer_id
+	 *
+	 * @return array
+	 * @throws \Exception
+	 */
+	public function process_standard_payment($order, $amount, $customer_id)
+	{
+		$order_id = PayplugWoocommerceHelper::is_pre_30() ? $order->id : $order->get_id();
+		try {
+			if (!$this->checkAmericanExpress()) {
+				throw new \Exception(__('Payment processing failed. Please retry.', 'payplug'));
+			}
+
+			$address_data = PayplugAddressData::from_order($order);
+
+			$return_url = esc_url_raw($order->get_checkout_order_received_url());
+
+			if (!(substr( $return_url, 0, 4 ) === "http")) {
+				$return_url = get_site_url().$return_url;
+			}
+
+			$payment_data = [
+				'amount'           => $amount,
+				'currency'         => get_woocommerce_currency(),
+				'payment_method'   => $this->id,
+				'billing'          => $address_data->get_billing(),
+				'shipping'         => $address_data->get_shipping(),
+				'hosted_payment'   => [
+					'return_url' => $return_url,
+					'cancel_url' => esc_url_raw($order->get_cancel_order_url_raw()),
+				],
+				'notification_url' => esc_url_raw(WC()->api_request_url('PayplugGateway')),
+				'metadata'         => [
+					'order_id'    => $order_id,
+					'customer_id' => ((int) $customer_id > 0) ? $customer_id : 'guest',
+					'domain'      => $this->limit_length(esc_url_raw(home_url()), 500),
+				],
+				"save_card"=> false,
+				"force_3ds"=> false
+			];
+
+			/**
+			 * Filter the payment data before it's used
+			 *
+			 * @param array $payment_data
+			 * @param int $order_id
+			 * @param array $customer_details
+			 * @param PayplugAddressData $address_data
+			 */
+			$payment_data = apply_filters('payplug_gateway_payment_data', $payment_data, $order_id, [], $address_data);
+			$payment      = $this->api->payment_create($payment_data);
+
+			// Save transaction id for the order
+			PayplugWoocommerceHelper::is_pre_30()
+				? update_post_meta($order_id, '_transaction_id', $payment->id)
+				: $order->set_transaction_id($payment->id);
+
+			if (is_callable([$order, 'save'])) {
+				$order->save();
+			}
+
+			/**
+			 * Fires once a payment has been created.
+			 *
+			 * @param int $order_id Order ID
+			 * @param PaymentResource $payment Payment resource
+			 */
+			\do_action('payplug_gateway_payment_created', $order_id, $payment);
+
+			$metadata = PayplugWoocommerceHelper::extract_transaction_metadata($payment);
+			PayplugWoocommerceHelper::save_transaction_metadata($order, $metadata);
+
+			PayplugGateway::log(sprintf('Payment creation complete for order #%s', $order_id));
+
+			return [
+				'result'   => 'success',
+				'redirect' => $payment->hosted_payment->payment_url,
+				'cancel'   => $payment->hosted_payment->cancel_url,
+			];
+
+		} catch (HttpException $e) {
+			PayplugGateway::log(sprintf('Error while processing order #%s : %s', $order_id, wc_print_r($e->getErrorObject(), true)), 'error');
+			throw new \Exception(__('Payment processing failed. Please retry.', 'payplug'));
+		} catch (\Exception $e) {
+			PayplugGateway::log(sprintf('Error while processing order #%s : %s', $order_id, $e->getMessage()), 'error');
+			throw new \Exception(__('Payment processing failed. Please retry.', 'payplug'));
+		}
+
+	}
 
 }
