6c95e94319e4aa1a71dd118d59d529ff92bdedc0
Djalel <ghabri.djalel@gmail.com>
2022-11-03 15:33:20 +0000

    WOOC-469 : WIP - Structure

--- src/Admin/Vue.php
+++ src/Admin/Vue.php
@@ -3,6 +3,8 @@
 namespace Payplug\PayplugWoocommerce\Admin;
 
 
+use Payplug\PayplugWoocommerce\Admin\Vue\Component;
+use Payplug\PayplugWoocommerce\Admin\Vue\PaymentMethods;
 use Payplug\PayplugWoocommerce\Controller\ApplePay;
 use Payplug\PayplugWoocommerce\Gateway\PayplugGateway;
 use Payplug\PayplugWoocommerce\Gateway\PayplugGatewayRequirements;
@@ -229,215 +231,14 @@ class Vue {
 				]
 			],
 			"options"      => [
-				$this->payment_method_standard(),
-				$this->payment_method_applepay(),
-				$this->payment_method_bancontact(),
-				$this->payment_method_amex()
+				(new PaymentMethods())->payment_method_standard(),
+				PaymentMethods::payment_method_applepay(),
+				PaymentMethods::payment_method_bancontact(),
+				PaymentMethods::payment_method_amex()
 			]
 		];
 
 		return $section;
-	}
-
-	/**
-	 * @param $text
-	 * @param $url
-	 * @param $target
-	 *
-	 * @return array
-	 */
-	public function link_component( $text, $url, $target ) {
-		return [
-			"text"   => $text,
-			"url"    => $url,
-			"target" => $target
-		];
-	}
-
-	/**
-	 * @param $active
-	 *
-	 * @return array
-	 */
-	public function payment_method_standard( $active = false ) {
-		return [
-			"type"         => "payment_method",
-			"name"         => "standard",
-			"title"        => __( 'payplug_section_standard_payment_title', 'payplug' ),
-			"image"        => esc_url( PAYPLUG_GATEWAY_PLUGIN_URL . 'assets/images/logos_scheme_CB.svg' ),
-			"checked"      => $active,
-			"descriptions" => [
-				"live"    => [
-					"description"      => __( 'payplug_section_standard_payment_description', 'payplug' ),
-					"advanced_options" => __( 'payplug_section_standard_payment_advanced_options_label', 'payplug' ),
-				],
-				"sandbox" => [
-					"description"      => __( 'payplug_section_standard_payment_description', 'payplug' ),
-					"advanced_options" => __( 'payplug_section_standard_payment_advanced_options_label', 'payplug' ),
-				]
-			],
-			"options"      => [
-				$this->embeded_option(),
-				$this->one_click_option()
-			]
-		];
-	}
-
-	/**
-	 * @param $active
-	 *
-	 * @return array|bool[]|false[]
-	 */
-	public function one_click_option( $active = null ) {
-		$option = [
-			"type"         => "payment_option",
-			"sub_type"     => "switch",
-			"name"         => "one_click",
-			"title"        => __( 'payplug_section_one_click_option_title', 'payplug' ),
-			"descriptions" => [
-				"live"    => [
-					"description"    => __( 'payplug_section_one_click_option_description', 'payplug' ),
-					"link_know_more" => $this->link_component(
-						__( 'payplug_section_one_click__know_more', 'payplug' ),
-						"https://support.payplug.com/hc/en-gb/articles/4409698334098",
-						"_blank" ),
-				],
-				"sandbox" => [
-					"description"    => __( 'payplug_section_one_click_option_description', 'payplug' ),
-					"link_know_more" => $this->link_component(
-						__( 'payplug_section_one_click__know_more', 'payplug' ),
-						"https://support.payplug.com/hc/en-gb/articles/4409698334098",
-						"_blank" ),
-				]
-			]
-		];
-		if (isset(get_option( 'woocommerce_payplug_settings', [] )['oneclick'])) {
-			if (get_option( 'woocommerce_payplug_settings', [] )['oneclick'] != 'no') {
-				$option = $option + ["checked" => true];
-			} elseif (get_option( 'woocommerce_payplug_settings', [] )['oneclick'] == 'no') {
-				$option = $option + ["checked" => false];
-			}
-
-		}
-		return $option;
-	}
-
-	/**
-	 * @return array
-	 */
-	public function embeded_option() {
-		$option = (get_option( 'woocommerce_payplug_settings', [] )['payment_method'] != "") ? get_option( 'woocommerce_payplug_settings', [] )['payment_method'] : false;
-		return [
-			"type"         => "payment_option",
-			"sub_type"     => "IOptions",
-			"name"         => "embeded",
-			"title"        => __( 'payplug_section_standard_payment_option_title', 'payplug' ),
-			"descriptions" => [
-				"live"    => [
-					"description_redirected"    => __( 'payplug_section_standard_payment_redirected_description', 'payplug' ),
-					"description_popup"    => __( 'payplug_section_standard_payment_popup_description', 'payplug' ),
-					"link_know_more" => $this->link_component(
-						__( 'payplug_section_standard_payment_know_more', 'payplug' ),
-						"https://support.payplug.com/hc/en-gb/articles/4409698334098",
-						"_blank" ),
-				],
-				"sandbox" => [
-					"description_redirected"    => __( 'payplug_section_standard_payment_redirected_description', 'payplug' ),
-					"description_popup"    => __( 'payplug_section_standard_payment_popup_description', 'payplug' ),
-					"link_know_more" => $this->link_component(
-						__( 'payplug_section_standard_payment_know_more', 'payplug' ),
-						"https://support.payplug.com/hc/en-gb/articles/4409698334098",
-						"_blank" ),
-				]
-			],
-			"options"      => [
-				[
-					"name"  => "payplug_embedded",
-					"label" => __( 'payplug_section_standard_payment_option_popup_label', 'payplug' ),
-					"value" => "popup"
-				],
-				[
-					"name"    => "payplug_embedded",
-					"label"   => __( 'payplug_section_standard_payment_option_redirected_label', 'payplug' ),
-					"value"   => "redirected"
-				]
-			]
-		];
-	}
-
-	/**
-	 * @param $active
-	 *
-	 * @return array
-	 */
-	public function payment_method_applepay( $active = false ) {
-		return [
-			"type" => "payment_method",
-			"name" => "applepay",
-			"title" => __( 'payplug_section_applepay_payment_title', 'payplug' ),
-			"image" => esc_url( PAYPLUG_GATEWAY_PLUGIN_URL . 'assets/images/apple-pay-checkout.svg' ),
-			"checked" =>  $active,
-			"descriptions" => [
-				"live"    => [
-					"description"      => __( 'payplug_section_applepay_payment_description', 'payplug' ),
-					"link_know_more" => $this->link_component(__( 'payplug_know_more_label', 'payplug' ),"https://support.payplug.com/hc/en-gb/articles/5149384347292", "_blank"),
-				],
-				"sandbox" => [
-					"description"      => __( 'payplug_apple_pay_testmode_description', 'payplug' ),
-					"link_know_more" => $this->link_component(__( 'payplug_know_more_label', 'payplug' ),"https://support.payplug.com/hc/en-gb/articles/5149384347292", "_blank"),
-				]
-			],
-		];
-	}
-
-	/**
-	 * @param $active
-	 *
-	 * @return array
-	 */
-	public function payment_method_bancontact( $active = false ) {
-		return [
-			"type" => "payment_method",
-			"name" => "bancontact",
-			"title" => __( 'payplug_section_bancontact_payment_title', 'payplug' ),
-			"image" => esc_url( PAYPLUG_GATEWAY_PLUGIN_URL . 'assets/images/lg-bancontact-checkout.png' ),
-			"checked" =>  $active,
-			"descriptions" => [
-				"live"    => [
-					"description"      => __( 'payplug_section_bancontact_payment_description', 'payplug' ),
-					"link_know_more" => $this->link_component(__( 'payplug_know_more_label', 'payplug' ),"https://support.payplug.com/hc/en-gb/articles/4408157435794", "_blank"),
-				],
-				"sandbox" => [
-					"description"      => __( 'payplug_section_applepay_payment_description', 'payplug' ),
-					"link_know_more" => $this->link_component(__( 'payplug_know_more_label', 'payplug' ),"https://support.payplug.com/hc/en-gb/articles/4408157435794", "_blank"),
-				]
-			],
-		];
-	}
-
-	/**
-	 * @param $active
-	 *
-	 * @return array
-	 */
-	public function payment_method_amex( $active = false ) {
-		return [
-			"type" => "payment_method",
-			"name" => "american_express",
-			"title" => __( 'payplug_section_american_express_payment_title', 'payplug' ),
-			"image" => esc_url( PAYPLUG_GATEWAY_PLUGIN_URL . 'assets/images/lg-american_express-checkout.png' ),
-			"checked" =>  $active,
-			"descriptions" => [
-				"live"    => [
-					"description"      => __( 'payplug_section_american_express_payment_description', 'payplug' ),
-					"link_know_more" => $this->link_component(__( 'payplug_know_more_label', 'payplug' ),"https://support.payplug.com/hc/en-gb/articles/4408157435794", "_blank"),
-				],
-				"sandbox" => [
-					"description"      => __( 'payplug_section_applepay_payment_description', 'payplug' ),
-					"link_know_more" => $this->link_component(__( 'payplug_know_more_label', 'payplug' ),"https://support.payplug.com/hc/en-gb/articles/4408157435794", "_blank"),
-				]
-			],
-		];
 	}
 
 	/**
@@ -465,11 +266,11 @@ class Vue {
 				"descriptions" => [
 					"live"    => [
 						"description"      => __( 'payplug_section_bancontact_payment_description', 'payplug' ),
-						"link_know_more" => $this->link_component(__( 'payplug_section_bancontact_payment_know_more_label', 'payplug' ),"https://support.payplug.com/hc/en-gb/articles/360013071080", "_blank"),
+						"link_know_more" => Component::link(__( 'payplug_section_bancontact_payment_know_more_label', 'payplug' ),"https://support.payplug.com/hc/en-gb/articles/360013071080", "_blank"),
 					],
 					"sandbox" => [
 						"description"      => __( 'payplug_section_applepay_payment_description', 'payplug' ),
-						"link_know_more" => $this->link_component(__( 'payplug_section_bancontact_payment_know_more_label', 'payplug' ),"https://support.payplug.com/hc/en-gb/articles/360013071080", "_blank"),
+						"link_know_more" => Component::link(__( 'payplug_section_bancontact_payment_know_more_label', 'payplug' ),"https://support.payplug.com/hc/en-gb/articles/360013071080", "_blank"),
 					]
 				],
 				"options" => [
@@ -548,6 +349,10 @@ class Vue {
 		];
 	}
 
+
+	/**
+	 * @return array
+	 */
 	public function payplug_section_status() {
 		$payplug_requirements = new PayplugGatewayRequirements(new PayplugGateway());
 
