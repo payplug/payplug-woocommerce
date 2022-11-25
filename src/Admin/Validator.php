966ce5e7be423a9d58b08dc8aef9ce87a648f7d9
Djalel <ghabri.djalel@gmail.com>
2022-11-25 16:28:34 +0000

    WIP : WOOC-607 : changing data validators

--- src/Admin/Validator.php
+++ src/Admin/Validator.php
@@ -6,20 +6,24 @@ class Validator {
 
 	public static function enabled($value) {
 		if (isset($value) && !empty($value)) {
-			if (in_array($value, ['yes', 'no'])) {
+			if ($value)
 				return true;
-			}
+			elseif ($value == false)
+				return false;
 		}
-		return false;
+		http_response_code(400);
+		wp_send_json_error(['error' => 'enabled is missing']);
 	}
 
 	public static function mode($value) {
 		if (isset($value) && !empty($value)) {
-			if (in_array($value, ['yes', 'no'])) {
+			if ($value)
 				return true;
-			}
+			elseif ($value == false)
+				return false;
 		}
-		return false;
+		http_response_code(400);
+		wp_send_json_error(['error' => 'mode is missing']);
 	}
 
 	public static function payment_method($value) {
@@ -33,38 +37,46 @@ class Validator {
 
 	public static function debug($value) {
 		if (isset($value) && !empty($value)) {
-			if (in_array($value, ['yes', 'no'])) {
+			if ($value)
 				return true;
-			}
+			elseif ($value == false)
+				return false;
 		}
-		return false;
+		http_response_code(400);
+		wp_send_json_error(['error' => 'debug is missing']);
 	}
 
 	public static function oneclick($value) {
 		if (isset($value) && !empty($value)) {
-			if (in_array($value, ['yes', 'no'])) {
+			if ($value)
 				return true;
-			}
+			elseif ($value == false)
+				return false;
 		}
-		return false;
+		http_response_code(400);
+		wp_send_json_error(['error' => 'oneclick is missing']);
 	}
 
 	public static function bancontact($value) {
 		if (isset($value) && !empty($value)) {
-			if (in_array($value, ['yes', 'no'])) {
+			if ($value)
 				return true;
-			}
+			elseif ($value == false)
+				return false;
 		}
-		return false;
+		http_response_code(400);
+		wp_send_json_error(['error' => 'bancntact is missing']);
 	}
 
 	public static function apple_pay($value) {
 		if (isset($value) && !empty($value)) {
-			if (in_array($value, ['yes', 'no'])) {
+			if ($value)
 				return true;
-			}
+			elseif ($value == false)
+				return false;
 		}
-		return false;
+		http_response_code(400);
+		wp_send_json_error(['error' => 'apple pay is missing']);
 	}
 
 	public static function american_express($value) {
@@ -78,11 +90,13 @@ class Validator {
 
 	public static function oney($value) {
 		if (isset($value) && !empty($value)) {
-			if (in_array($value, ['yes', 'no'])) {
+			if ($value)
 				return true;
-			}
+			elseif ($value == false)
+				return false;
 		}
-		return false;
+		http_response_code(400);
+		wp_send_json_error(['error' => 'oney is missing']);
 	}
 
 	public static function oney_type($value) {
