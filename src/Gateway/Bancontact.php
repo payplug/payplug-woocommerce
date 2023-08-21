45be248723b3a6bcb97b75fd9f08bb77c7221671
PPmmesquita <mmesquita-ext@payplug.com>
2023-08-21 09:42:07 +0100

    WOOC-967 bancontact refactor

---
+++ src/Gateway/Bancontact.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Payplug\PayplugWoocommerce\Gateway;
+
+use Payplug\Exception\HttpException;
+use Payplug\PayplugWoocommerce\Controller\PayplugGenericGateway;
+
+class Bancontact  extends PayplugGenericGateway
+{
+
+	public function __construct()
+	{
+
+		parent::__construct();
+
+		/** @var \WC_Settings_API  override $id */
+		$this->id                 = 'bancontact';
+
+		/** @var \WC_Payment_Gateway overwrite for bancontact settings  */
+		$this->method_title       = __('payplug_bancontact_title', 'payplug');
+		$this->method_description = __('payplug_bancontact_description', 'payplug');
+
+		$this->title = __('payplug_bancontact_title', 'payplug');
+		$this->description = '';
+		$this->has_fields = false;
+		$this->image = 'lg-bancontact-checkout.svg';
+
+		if(!$this->checkGateway()){
+			$this->enabled = 'no';
+		}
+
+	}
+
+}
