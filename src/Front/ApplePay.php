478991d7411829a715ecc9e5f535d43a04a3c2f1
Manuel Mesquita <mmesquita-ext@payplug.com>
2024-06-19 16:33:43 +0100

    WOOC-1324 - handle apple_pay_cart and apple_pay_checkout

--- src/Front/ApplePay.php
+++ src/Front/ApplePay.php
@@ -9,44 +9,12 @@ use function is_cart;
 class ApplePay {
 
 	public function __construct() {
-		add_action( 'woocommerce_after_cart_totals', [ $this, 'applepayButton' ] );
 
 		//routes for apple_pay on cart
 		add_action( 'wc_ajax_applepay_get_shippings', [ $this, 'applepay_get_shippings' ] );
 		add_action( 'wc_ajax_place_order_with_dummy_data', [ $this, 'place_order_with_dummy_data' ] );
 		add_action( 'wc_ajax_update_applepay_order', [ $this, 'update_applepay_order' ] );
 		add_action( 'wc_ajax_update_applepay_payment', [ $this, 'update_applepay_payment' ] );
-	}
-
-	/**
-	 * Add button and Scripts
-	 */
-	public function applepayButton() {
-
-		if (is_cart()) {
-			$apple_pay = new ApplePayGateway();
-			wp_enqueue_script( 'apple-pay-sdk', 'https://applepay.cdn-apple.com/jsapi/v1/apple-pay-sdk.js', array(), false, true );
-			wp_enqueue_script('payplug-apple-pay-cart', PAYPLUG_GATEWAY_PLUGIN_URL . 'assets/js/payplug-apple-pay-cart.js', ['jquery', 'apple-pay-sdk'], PAYPLUG_GATEWAY_VERSION, true);
-			wp_localize_script( 'payplug-apple-pay-cart', 'apple_pay_params',
-				array(
-					'ajax_url_applepay_get_shippings' => \WC_AJAX::get_endpoint('applepay_get_shippings'),
-					'ajax_url_place_order_with_dummy_data' => \WC_AJAX::get_endpoint('place_order_with_dummy_data'),
-					'ajax_url_update_applepay_order' => \WC_AJAX::get_endpoint('update_applepay_order'),
-					'ajax_url_update_applepay_payment' => \WC_AJAX::get_endpoint('update_applepay_payment'),
-					'ajax_url_applepay_get_order_totals' => \WC_AJAX::get_endpoint('applepay_get_order_totals'),
-
-					'countryCode' => WC()->customer->get_billing_country(),
-					'currencyCode' => get_woocommerce_currency(),
-					'apple_pay_domain' => $_SERVER['HTTP_HOST']
-				)
-			);
-			$apple_pay->add_apple_pay_css();
-
-			if ( $apple_pay->enabled ) {
-				echo $apple_pay->description;
-			}
-
-		}
 	}
 
 	public function applepay_get_shippings() {
