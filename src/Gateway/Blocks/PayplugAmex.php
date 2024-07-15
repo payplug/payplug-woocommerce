f8c0878bc141c327f0a1b1d4fe5c1b08cf5c7645
Djalel <ghabri.djalel@gmail.com>
2024-07-15 13:55:42 +0100

    American Express blocks & minor blocks fixes

---
+++ src/Gateway/Blocks/PayplugAmex.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Payplug\PayplugWoocommerce\Gateway\Blocks;
+
+class PayplugAmex extends PayplugGenericBlock
+{
+
+	/**
+	 * Payment method name/id/slug.
+	 *
+	 * @var string
+	 */
+	protected $name = "american_express";
+
+	/**
+	 * Returns an associative array of data to be exposed for the payment method's client side.
+	 */
+	public function get_payment_method_data() {
+		$data = parent::get_payment_method_data();
+		$data['icon'] =  [
+			"src" => esc_url(PAYPLUG_GATEWAY_PLUGIN_URL . '/assets/images/checkout/' . $this->gateway->image),
+			'icon_alt' => $data['name'],
+		];
+		return $data;
+	}
+
+
+
+
+
+
+}
