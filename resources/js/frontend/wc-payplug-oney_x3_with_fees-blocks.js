b7060cacf777297a36f6a489aa1330ac9412db10
Djalel <ghabri.djalel@gmail.com>
2024-07-31 12:02:36 +0100

    WOOC-1353 : Oney 3x with fees blocks

--- resources/js/frontend/wc-payplug-oney_x3_with_fees-blocks.js
+++ resources/js/frontend/wc-payplug-oney_x3_with_fees-blocks.js
@@ -1,88 +1,117 @@
-
-import { __ } from '@wordpress/i18n';
-import { registerPaymentMethod } from '@woocommerce/blocks-registry';
-import { decodeEntities } from '@wordpress/html-entities';
-import { getSetting } from '@woocommerce/settings';
-const { useSelect } = wp.data;
+import {__} from '@wordpress/i18n';
+import {registerPaymentMethod} from '@woocommerce/blocks-registry';
+import {decodeEntities} from '@wordpress/html-entities';
+import {getSetting} from '@woocommerce/settings';
 
 
-const settings = getSetting( 'oney_x3_with_fees_data', {} );
+
+const settings = getSetting('oney_x3_with_fees_data', {});
 const defaultLabel = __('Gateway method title', 'payplug');
-const label = decodeEntities( settings?.title ) || defaultLabel;
+const label = decodeEntities(settings?.title) || defaultLabel;
 
 let Content;
 
-let ContentDisabled = () => {
-	return (
-		<div>
-			aaaa
-		</div>
-
-	);
-}
-
 const translations = settings?.translations;
 
-const allowed_country_codes = settings?.allowed_country_codes;
+const allowed_country_codes = settings?.requirements.allowed_country_codes;
 
-console.log('allowed_country_codes : ' + JSON.stringify(allowed_country_codes))
+const oney_response = settings?.oney_response;
 
-if (settings?.oney_response) {
-	const down_payment_amount = decodeEntities( settings?.oney_response['x3_with_fees']['down_payment_amount'] )  || defaultLabel;
-	let total_price_oney = down_payment_amount;
-
+if (typeof oney_response.x3_with_fees !== 'undefined') {
+	var down_payment_amount = parseFloat(settings?.oney_response['x3_with_fees']['down_payment_amount']) ;
+	var total_price_oney = down_payment_amount;
 	settings?.oney_response['x3_with_fees']['installments'].forEach((amount) => {
 
-		total_price_oney += amount['amount'];
+		total_price_oney += parseFloat(amount['amount']);
 
-	})
+	});
 
-	const currency = decodeEntities( settings?.currency) ;
+
 	Content = (e) => {
-	//	console.log("Content : " +JSON.stringify(e));
+
 		let country = e.shippingData.shippingAddress.country;
 		if (allowed_country_codes.indexOf(country) === -1) {
+			settings.icon.class = 'disable-checkout-icons';
 			return (
-				<div className={settings?.oney_disabled.description.class}>
-					{settings?.oney_disabled.description.text}
+				<div className={settings?.oney_disabled.validations.country.class}>
+					{settings?.oney_disabled.validations.country.text}
+				</div>
+
+			);
+		} else if (e.cartData.cartItems.length > settings?.requirements.max_quantity) {
+			settings.icon.class = 'disable-checkout-icons';
+			return (
+				<div className={settings?.oney_disabled.validations.items_count.class}>
+					{settings?.oney_disabled.validations.items_count.text}
+				</div>
+
+			);
+		} else if ((e.billing.cartTotal.value > settings?.requirements.max_threshold)
+			|| (e.billing.cartTotal.value < settings?.requirements.min_threshold)) {
+			settings.icon.class = 'disable-checkout-icons';
+			return (
+				<div className={settings?.oney_disabled.validations.amount.class}>
+					{settings?.oney_disabled.validations.amount.text}
 				</div>
 
 			);
 		} else {
+			settings.icon.class = 'payplug-payment-icon';
 			return (
-				<p>
+				<div>
 					<div className="payplug-oney-flex">
 						<div>{translations['bring']} :</div>
-						<div>{down_payment_amount} {currency}</div>
+						<div>{down_payment_amount} {e.billing.currency.symbol}</div>
 					</div>
 					<div className="payplug-oney-flex">
-						<small>( {translations['oney_financing_cost']} <b>{settings?.oney_response['x3_with_fees']['total_cost']} {currency}</b> TAEG : <b>{settings?.oney_response['x3_with_fees']['effective_annual_percentage_rate']} %</b> )</small>
+						<small>( {translations['oney_financing_cost']}
+							<b>{settings?.oney_response['x3_with_fees']['total_cost']} {e.billing.currency.symbol}</b> TAEG
+							: <b>{settings?.oney_response['x3_with_fees']['effective_annual_percentage_rate']} %</b> )</small>
 					</div>
 					<div className="payplug-oney-flex">
 						<div>{translations['1st monthly payment']}:</div>
-						<div>{settings?.oney_response['x3_with_fees']['installments'][0]['amount']} {currency}</div>
+						<div>{settings?.oney_response['x3_with_fees']['installments'][0]['amount']} {e.billing.currency.symbol}</div>
 					</div>
 					<div className="payplug-oney-flex">
 						<div>{translations['2nd monthly payment']}:</div>
-						<div>{settings?.oney_response['x3_with_fees']['installments'][1]['amount']} {currency}</div>
+						<div>{settings?.oney_response['x3_with_fees']['installments'][1]['amount']} {e.billing.currency.symbol}</div>
 					</div>
 					<div className="payplug-oney-flex">
 						<div><b>{translations['oney_total']}</b></div>
-						<div><b>{total_price_oney} {currency}</b></div>
+						<div><b>{total_price_oney} {e.billing.currency.symbol}</b></div>
 					</div>
-				</p>
+				</div>
+			);
+		}
+	};
+} else {
+	// No oney response
+	Content = (e) => {
+		let country = e.shippingData.shippingAddress.country;
+		if (allowed_country_codes.indexOf(country) === -1) {
+			return (
+				<div className={settings?.oney_disabled.validations.country.class}>
+					{settings?.oney_disabled.validations.country.text}
+				</div>
+
+			);
+		} else if (e.cartData.cartItems.length > settings?.requirements.max_quantity) {
+			return (
+				<div className={settings?.oney_disabled.validations.items_count.class}>
+					{settings?.oney_disabled.validations.items_count.text}
+				</div>
+
+			);
+		} else if ((e.billing.cartTotal.value > settings?.requirements.max_threshold)
+			|| (e.billing.cartTotal.value < settings?.requirements.min_threshold)) {
+			return (
+				<div className={settings?.oney_disabled.validations.amount.class}>
+					{settings?.oney_disabled.validations.amount.text}
+				</div>
+
 			);
 		}
 
-	};
-} else {
-	Content = () => {
-		return (
-			<div className={settings?.description.class}>
-				{settings?.description.text}
-			</div>
-
-		);
 	};
 }
 
@@ -90,19 +119,19 @@ if (settings?.oney_response) {
  * Label component
  *
  */
-const Label = (e) => {
-	console.log("label : " +JSON.stringify(e));
+const Label = () => {
 	return (
-		<span style={{ width: '100%' }}>
+		<span style={{width: '100%'}}>
             {label}
-			<Icon />
+			<Icon/>
         </span>
 	)
 }
 
 const Icon = () => {
 	return (
-		<img src={settings?.icon.src} alt={settings?.icon.alt} className={settings?.icon.class} style={{float: 'right'}}/>
+		<img src={settings?.icon.src} alt={settings?.icon.alt} className={settings.icon.class}
+			 style={{float: 'right'}}/>
 	)
 }
 
@@ -111,16 +140,10 @@ const Icon = () => {
  */
 let oney_x3_with_fees = {
 	name: "oney_x3_with_fees",
-	label: <Label />,
-	content: <Content />,
-	edit: <Content />,
-	canMakePayment: (e) => {
-		let cart_total = e.cartTotals.total_price;
-		let country = e.shippingAddress.country;
-		if (allowed_country_codes.indexOf(country) === -1) {
-			//return false;
-		}
-		//console.log("cart totals :" + JSON.stringify(e.cartTotals.total_price));
+	label: <Label/>,
+	content: <Content/>,
+	edit: <Content/>,
+	canMakePayment: () => {
 		return true;
 	},
 	ariaLabel: label,
@@ -129,5 +152,5 @@ let oney_x3_with_fees = {
 	},
 };
 
-registerPaymentMethod( oney_x3_with_fees );
+registerPaymentMethod(oney_x3_with_fees);
 
