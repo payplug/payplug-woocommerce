1882f9f003f97d08ecfa0fd5b6b1a4a2aa5385b2
Djalel <ghabri.djalel@gmail.com>
2024-08-02 15:04:34 +0100

    WOOC-1353 : Onet blocks backend architecture

--- src/Gateway/Blocks/PayplugOney3x.php
+++ src/Gateway/Blocks/PayplugOney3x.php
@@ -18,8 +18,6 @@ class PayplugOney3x extends PayplugGenericBlock {
 	protected $total_price;
 
 
-
-
 	public function check_oney() {
 
 		$products_qty = (int) $this->cart->cart_contents_count;
@@ -27,7 +25,7 @@ class PayplugOney3x extends PayplugGenericBlock {
 		if ( $this->total_price < $this->gateway->oney_thresholds_min || $this->total_price > $this->gateway->oney_thresholds_max ) {
 			$this->description = [
 				'text'  => sprintf( __( 'The total amount of your order should be between %s€ and %s€ to pay with Oney.', 'payplug' ), $this->gateway->oney_thresholds_min, $this->gateway->oney_thresholds_max ),
-				'class' => 'payment_method_oney_x3_with_fees_disabled'
+				'class' => 'payment_method_' . $this->name . '_disabled'
 			];
 
 			return false;
@@ -37,7 +35,7 @@ class PayplugOney3x extends PayplugGenericBlock {
 		if ( $products_qty >= $this->gateway::ONEY_PRODUCT_QUANTITY_MAXIMUM ) {
 			$this->description = [
 				'text'  => sprintf( __( 'The payment with Oney is unavailable because you have more than %s items in your cart.', 'payplug' ), $this->gateway::ONEY_PRODUCT_QUANTITY_MAXIMUM ),
-				'class' => 'payment_method_oney_x3_with_fees_disabled'
+				'class' => 'payment_method_' . $this->name . '_disabled'
 			];
 
 			return $this->gateway::ONEY_UNAVAILABLE_CODE_CART_SIZE_TOO_HIGH;
@@ -53,7 +51,7 @@ class PayplugOney3x extends PayplugGenericBlock {
 		if ( ! $this->gateway->validate_shipping_billing_country( $country_code_shipping, $country_code_billing ) ) {
 			$this->description = [
 				'text'  => __( 'Unavailable for the specified country.', 'payplug' ),
-				'class' => 'payment_method_oney_x3_with_fees_disabled'
+				'class' => 'payment_method_' . $this->name . '_disabled'
 			];
 
 			return false;
@@ -66,20 +64,21 @@ class PayplugOney3x extends PayplugGenericBlock {
 
 		$data                  = parent::get_payment_method_data();
 		$data['icon']          = [
-			'src' => esc_url(PAYPLUG_GATEWAY_PLUGIN_URL . '/assets/images/checkout/'.$this->icon),
+			'src'   => esc_url( PAYPLUG_GATEWAY_PLUGIN_URL . '/assets/images/checkout/' . $this->icon ),
 			'class' => 'payplug-payment-icon',
-			'alt' => $this->gateway->title
+			'alt'   => $this->gateway->title
 		];
 		$data['description']   = $this->gateway->description;
-		$data['oney_response'] = $this->gateway->api->simulate_oney_payment($this->total_price, 'with_fees');
-		$data['currency']      = get_woocommerce_currency_symbol( get_option( 'woocommerce_currency' ) );
+		$data['oney_response'] = $this->gateway->api->simulate_oney_payment( $this->total_price, 'with_fees' );
+
+		$data['currency'] = get_woocommerce_currency_symbol( get_option( 'woocommerce_currency' ) );
 
 		$data['translations']['bring']               = __( 'Bring', 'payplug' );
 		$data['translations']['oney_financing_cost'] = __( 'oney_financing_cost', 'payplug' );
 		$data['translations']['1st monthly payment'] = __( '1st monthly payment', 'payplug' );
 		$data['translations']['2nd monthly payment'] = __( '2nd monthly payment', 'payplug' );
 		$data['translations']['oney_total']          = __( 'oney_total', 'payplug' );
-		$data['allowed_country_codes'] = $this->gateway->allowed_country_codes;
+		$data['allowed_country_codes']               = $this->gateway->allowed_country_codes;
 
 		$data['requirements'] = [
 			'max_quantity'          => $this->gateway::ONEY_PRODUCT_QUANTITY_MAXIMUM,
@@ -96,22 +95,21 @@ class PayplugOney3x extends PayplugGenericBlock {
 	public function oney_disabled() {
 		$data                     = parent::get_payment_method_data();
 		$disable                  = 'disable-checkout-icons';
-		$available_img            = 'x3_with_fees.svg';
-		$data['icon']['src']      = esc_url( PAYPLUG_GATEWAY_PLUGIN_URL . '/assets/images/checkout/' . $available_img );
+		$data['icon']['src']      = esc_url( PAYPLUG_GATEWAY_PLUGIN_URL . '/assets/images/checkout/' . $this->icon );
 		$data['icon']['class']    = "payplug-payment-icon ' . $disable . '";
 		$data['icon']['icon_alt'] = $this->gateway->title;
 		$data['validations']      = [
-			'amount' => [
+			'amount'      => [
 				'text'  => sprintf( __( 'The total amount of your order should be between %s€ and %s€ to pay with Oney.', 'payplug' ), $this->gateway->oney_thresholds_min, $this->gateway->oney_thresholds_max ),
-				'class' => 'payment_method_oney_x3_with_fees_disabled'
+				'class' => 'payment_method_' . $this->name . '_disabled'
 			],
-			'country' => [
+			'country'     => [
 				'text'  => __( 'Unavailable for the specified country.', 'payplug' ),
-				'class' => 'payment_method_oney_x3_with_fees_disabled'
+				'class' => 'payment_method_' . $this->name . '_disabled'
 			],
 			'items_count' => [
 				'text'  => sprintf( __( 'The payment with Oney is unavailable because you have more than %s items in your cart.', 'payplug' ), $this->gateway::ONEY_PRODUCT_QUANTITY_MAXIMUM ),
-				'class' => 'payment_method_oney_x3_with_fees_disabled'
+				'class' => 'payment_method_' . $this->name . '_disabled'
 			]
 		];
 
@@ -123,12 +121,11 @@ class PayplugOney3x extends PayplugGenericBlock {
 	 */
 	public function get_payment_method_data() {
 
-		if (is_checkout()) {
+		if ( is_checkout() ) {
 			$this->cart = WC()->cart;
 
-			$this->total_price = floatval(WC()->cart->total);
+			$this->total_price = floatval( WC()->cart->total );
 		}
-
 
 
 		return $this->oney_enabled();
