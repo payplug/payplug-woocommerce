de57d5d8d5e5c6fb996b08a1fc4880a85762dd89
Manuel Mesquita <mmesquita-ext@payplug.com>
2024-08-31 01:37:06 +0100

    WOOC-1355 - IP Submit, error handling, etc

--- resources/js/frontend/wc-payplug-integratedPayment-blocks.js
+++ resources/js/frontend/wc-payplug-integratedPayment-blocks.js
@@ -1,82 +1,122 @@
 import { getSetting } from '@woocommerce/settings';
-import { useEffect } from 'react';
-import {getPayment, check_payment} from "./helper/wc-payplug-requests";
+import React, { useEffect, useRef } from 'react';
+import {
+	getPayment,
+	check_payment,
+	usePaymentFailHandler,
+	usePaymentCompleteHandler
+} from "./helper/wc-payplug-requests";
 const settings = getSetting( 'payplug_data', {} );
 
-export const IntegratedPayment = (
-	{settings: settings, props: props }
-) => {
+export const IntegratedPayment = ({props: props,}) => {
 
-	const { eventRegistration } = props;
-	const { onCheckoutValidation, onPaymentSetup } = eventRegistration;
+	const shouldSavePayment = false;
+	const { eventRegistration, emitResponse } = props;
+	const { onCheckoutValidation, onPaymentSetup, onCheckoutFail, onCheckoutSuccess } = eventRegistration;
 
 	useEffect(() => {
-
-		// Create an instance of Integrated Payments
 		ObjIntegratedPayment.api = new Payplug.IntegratedPayment(false);
 		ObjIntegratedPayment.api.setDisplayMode3ds(Payplug.DisplayMode3ds.LIGHTBOX)
-
-		// Add each payments fields
 		ObjIntegratedPayment.form.cardHolder = ObjIntegratedPayment.api.cardHolder(document.querySelector('.cardHolder-input-container'), {default: ObjIntegratedPayment.inputStyle.default, placeholder: settings?.payplug_integrated_payment_cardholder } );
 		ObjIntegratedPayment.form.pan = ObjIntegratedPayment.api.cardNumber(document.querySelector('.pan-input-container'), {default: ObjIntegratedPayment.inputStyle.default, placeholder: settings?.payplug_integrated_payment_card_number } );
 		ObjIntegratedPayment.form.cvv = ObjIntegratedPayment.api.cvv(document.querySelector('.cvv-input-container'), {default: ObjIntegratedPayment.inputStyle.default, placeholder: settings?.payplug_integrated_payment_cvv } );
-
-		// With one field for expiration date
 		ObjIntegratedPayment.form.exp = ObjIntegratedPayment.api.expiration(document.querySelector('.exp-input-container'), {default: ObjIntegratedPayment.inputStyle.default, placeholder: settings?.payplug_integrated_payment_expiration_date } );
 		ObjIntegratedPayment.scheme = ObjIntegratedPayment.api.getSupportedSchemes();
-
-		ObjIntegratedPayment.api.onValidateForm( async ({isFormValid}) => {
-			if(isFormValid){
-				const payment = await getPayment(props);
-				ObjIntegratedPayment.paymentId = payment.payment_id;
-				ObjIntegratedPayment.return_url = payment.redirect;
-				await ObjIntegratedPayment.api.pay(ObjIntegratedPayment.paymentId, Payplug.Scheme.AUTO, {save_card: false});
-			}
-			return false;
-		});
-
-		ObjIntegratedPayment.api.onCompleted( function (event) {
-			const data = {'payment_id' : event.token};
-			console.log(data);
-
-			check_payment(data).then( () => {
-
-				console.log("paid");
-				console.log(ObjIntegratedPayment.return_url);
-
-				window.location.href = ObjIntegratedPayment.return_url;
-			})
-			.catch((error) => {
-				//TODO:: handling errors
-			});
-		});
-
 		fieldValidation();
 
 	}, []);
 
-
 	useEffect(() => {
-		const onValidation = () => {
+		const onValidation = async () => {
 			ObjIntegratedPayment.api.validateForm();
+
+			let isValid = false;
+			await validateForm().then( (response) => {
+				isValid = response;
+			});
+
+			if(!isValid){
+				return {
+					errorMessage: settings?.payplug_invalid_form
+				}
+			}else{
+				return isValid;
+			}
+
+			function validateForm(){
+				return new Promise(async (resolve, reject) => {
+					await ObjIntegratedPayment.api.onValidateForm(({isFormValid}) => {
+						resolve(isFormValid);
+					});
+				})
+			}
 		}
 		const unsubscribeAfterProcessing = onCheckoutValidation(onValidation);
 		return () => { unsubscribeAfterProcessing(); };
-
 	}, [onCheckoutValidation]);
 
-	useEffect(() => {
-		const onPayment = () => {
-			return { type: 'error' };
-		};
 
-		const unsubscribeAfterProcessing = onPaymentSetup(onPayment);
-		return () => {
-			unsubscribeAfterProcessing();
-		};
+	useEffect(() => {
+		const handlePaymentProcessing = async () => {
+			let data = {};
+			await getPayment(props).then( async (response) => {
+				ObjIntegratedPayment.paymentId = response.payment_id;
+				data = {'payment_id': response.payment_id};
+				ObjIntegratedPayment.return_url = response.redirect;
+				try {
+					await ObjIntegratedPayment.api.pay(ObjIntegratedPayment.paymentId, Payplug.Scheme.AUTO, {save_card: false});
+
+				}catch(error) {
+					return {
+						type: 'error',
+						message: error.message
+					}
+				}
+
+			});
+
+			let result = {};
+			await CompleteProcessingPayment().then( (res) => {
+				if(res.success){
+					result = {
+						type: 'success',
+						meta: {}
+					}
+				}else{
+					result = {
+						type: 'error',
+						message: res.data.message,
+					};
+				}
+			});
+
+			return result;
+
+			function CompleteProcessingPayment(){
+				return new Promise(async (resolve, reject) => {
+					await ObjIntegratedPayment.api.onCompleted( function (event) {
+						const data = {'payment_id' : event.token};
+						check_payment(data).then( (res) => {
+							resolve(res);
+						});
+					});
+				})
+			}
+
+		}
+		const unsubscribeAfterProcessing = onPaymentSetup(handlePaymentProcessing);
+		return () => { unsubscribeAfterProcessing(); };
+
 	}, [
-		onPaymentSetup
+		onPaymentSetup,
+		emitResponse.noticeContexts.PAYMENTS,
+		emitResponse.responseTypes.ERROR,
+		emitResponse.responseTypes.SUCCESS
 	]);
+
+	usePaymentFailHandler(onCheckoutFail, emitResponse);
+
+	usePaymentCompleteHandler(onCheckoutSuccess, emitResponse, shouldSavePayment)
 
 	const fieldValidation = () => {
 		jQuery.each(ObjIntegratedPayment.form, function (key, field) {
