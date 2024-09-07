f2655a5d8b504d8a3a868a2a378b8e717bdfdc35
Manuel Mesquita <mmesquita-ext@payplug.com>
2024-09-07 02:07:16 +0100

    WOOC-1354 - bugfix for popup mode

--- resources/js/frontend/wc-payplug-popup-blocks.js
+++ resources/js/frontend/wc-payplug-popup-blocks.js
@@ -2,38 +2,26 @@ import { getSetting } from '@woocommerce/settings';
 import React, { useEffect, useRef } from 'react';
 import { useSelect } from '@wordpress/data';
 import {check_payment, createOrder, getPayment} from "./helper/wc-payplug-requests";
+import {apple_pay_update_payment} from "./helper/wc-payplug-apple_pay-requests";
 const settings = getSetting( 'payplug_data', {} );
 
 const Popup = ({props: props,}) => {
 	const { eventRegistration, emitResponse, shouldSavePayment } = props;
-	const { onCheckoutValidation, onPaymentSetup, onCheckoutSuccess } = eventRegistration;
+	const { onPaymentSetup, onCheckoutSuccess } = eventRegistration;
 	const { PAYMENT_STORE_KEY, CHECKOUT_STORE_KEY } = window.wc.wcBlocksData;
 	const order_id = useSelect( ( select ) => select( CHECKOUT_STORE_KEY ).getOrderId() );
+	let getPaymentData;
 
 	useEffect((event) => {
 		const handlePaymentProcessing = async () => {
-
-			console.log(order_id);
-
 			let result = {};
+
 			await getPayment(props, order_id).then( async (response) => {
-				await showPopupPayment(response)
+				getPaymentData = response;
 			})
 
 			return {
-				type: 'error'
-			}
-
-			function showPopupPayment(response) {
-				return new Promise(async (resolve, reject) => {
-					try {
-						await Payplug.showPayment(response.data.redirect);
-						resolve();
-
-					} catch (e) {
-						reject(e);
-					}
-				})
+				type: 'success'
 			}
 		}
 		const unsubscribeAfterProcessing = onPaymentSetup(handlePaymentProcessing);
@@ -45,6 +33,42 @@ const Popup = ({props: props,}) => {
 		emitResponse.noticeContexts.PAYMENTS,
 		emitResponse.responseTypes.ERROR,
 		emitResponse.responseTypes.SUCCESS
+	]);
+
+	useEffect(() => {
+		const handlePaymentProcessing = async ({processingResponse: {paymentDetails}}) => {
+
+			console.log( "####### onCheckoutSuccess ######## ");
+			console.log( getPaymentData);
+
+			await showPopupPayment(getPaymentData).then( () => {
+				setTimeotu(function(){
+					return {
+						type: "error",
+						message: "Timeout",
+						messageContext: emitResponse.noticeContexts.PAYMENTS
+					}
+				},60000)
+			})
+
+			function showPopupPayment(getPaymentData) {
+				return new Promise(async (resolve, reject) => {
+					try {
+						await Payplug.showPayment(getPaymentData.data.redirect);
+						resolve();
+
+					} catch (e) {
+						reject(e);
+					}
+				})
+			}
+
+		}
+		const unsubscribeAfterProcessing = onCheckoutSuccess(handlePaymentProcessing);
+		return () => { unsubscribeAfterProcessing(); };
+
+	}, [
+		onCheckoutSuccess
 	]);
 
 	return (
