1bd76cc53f2cf35767ddcef887e5f880b2662cfa
Manuel Mesquita <mmesquita-ext@payplug.com>
2024-09-09 18:08:06 +0100

    WOOC-1353 delete compiled files

--- assets/js/blocks/wc-payplug-bancontact-blocks.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '4989a90794a88bbca193');
--- assets/js/blocks/wc-payplug-oney_x3_without_fees-blocks.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'dc8d2279cb415035424e');
