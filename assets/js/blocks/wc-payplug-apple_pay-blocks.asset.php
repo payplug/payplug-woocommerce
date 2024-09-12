cdc11fb68a8889f96b4ce789afd7a45bf21c5ab4
Manuel Mesquita <mmesquita-ext@payplug.com>
2024-09-12 15:34:27 +0100

    WOOC-1230 - build blocks files

---
+++ assets/js/blocks/wc-payplug-apple_pay-blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '97d58f58e23b23c8e855');
---
+++ assets/js/blocks/wc-payplug-oney_x3_without_fees-blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '8b58aafa2748cf45a448');
