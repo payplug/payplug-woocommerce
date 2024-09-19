3935d6b74f773ee167a678424638ca0fd96b404d
Djalel <ghabri.djalel@gmail.com>
2024-09-19 10:01:19 +0100

    WOOC-1230 : Build

--- assets/js/blocks/wc-payplug-american_express-blocks.asset.php
+++ assets/js/blocks/wc-payplug-american_express-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '76077961ece03dab40a6');
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '7d47745fdf31df3fbe2d');
