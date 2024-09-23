4effeb147d4fd1f13f8b2aa561721c51a18bddf7
Djalel <ghabri.djalel@gmail.com>
2024-09-23 10:58:31 +0100

    WOOC-1230 : Build

--- assets/js/blocks/wc-payplug-apple_pay-blocks.asset.php
+++ assets/js/blocks/wc-payplug-apple_pay-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '0d7ec3a9ebfe0733ff8a');
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'a0b0d23e2053613c6029');
--- assets/js/blocks/wc-payplug-payplug-blocks.asset.php
+++ assets/js/blocks/wc-payplug-payplug-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'c43939bcb38887540cff');
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'a319886806a6b97e95b1');
