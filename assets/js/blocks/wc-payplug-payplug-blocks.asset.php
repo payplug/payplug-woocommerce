ecad183ae1981f3cb05499a71cc7bdb43221b010
Manuel Mesquita <mmesquita-ext@payplug.com>
2024-09-30 09:21:15 +0100

    Build blocks

--- assets/js/blocks/wc-payplug-payplug-blocks.asset.php
+++ assets/js/blocks/wc-payplug-payplug-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'a319886806a6b97e95b1');
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '6ffbb4c93493ad8db5af');
