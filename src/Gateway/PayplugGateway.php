efa8da640e7677a25fa7a8ca863f23ab0942aec3
Djalel <ghabri.djalel@gmail.com>
2024-10-03 15:10:29 +0100

    WOOC-1441 : Fix order update status

--- src/Gateway/PayplugGateway.php
+++ src/Gateway/PayplugGateway.php
@@ -252,7 +252,7 @@ class PayplugGateway extends WC_Payment_Gateway_CC
             return;
         }
 
-		$order_id = ($id) ? : $_GET['order-received'];
+		$order_id = $_GET['order_id'];
 
 		if (empty($order_id)) {
 			$order_id = wc_get_order_id_by_order_key(wc_clean( (!empty($_GET['key']) ? $_GET['key'] : $id) ) );
@@ -840,7 +840,7 @@ class PayplugGateway extends WC_Payment_Gateway_CC
 					ob_clean();
 				}
 
-				$return_url = esc_url_raw($order->get_checkout_order_received_url());
+				$return_url = esc_url_raw($order->get_checkout_order_received_url()).'&order_id='.$order->get_id();
 
 				return array(
 					'payment_id' => $payment->id,
