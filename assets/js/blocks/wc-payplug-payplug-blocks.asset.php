43e2635633b97432592beb9c6ff27e89871a02f9
Manuel Mesquita <mmesquita-ext@payplug.com>
2024-10-07 14:17:57 +0100

    WOOC-1441 build files from last development

--- assets/js/blocks/wc-payplug-payplug-blocks.asset.php
+++ assets/js/blocks/wc-payplug-payplug-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'd34b310c5dc7244fbed9');
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '7a1e5ba0d1caa1394961');
