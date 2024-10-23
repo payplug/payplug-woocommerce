62d29f2c3fd75926731e8600f0d05743cbd4bd38
Manuel Mesquita <mmesquita-ext@payplug.com>
2024-10-23 13:24:14 +0100

    remove typescript files

--- assets/js/blocks/wc-payplug-oney_x3_with_fees-blocks.asset.php
+++ assets/js/blocks/wc-payplug-oney_x3_with_fees-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'cd0e758e76af86ee371e');
+<?php return array('dependencies' => array('wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '60b5d2df13f971e99dcf');
--- assets/js/blocks/wc-payplug-payplug-blocks.asset.php
+++ assets/js/blocks/wc-payplug-payplug-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '8dc81d145ce22e4938c9');
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'f6bd9e7f4793826e4c7a');
