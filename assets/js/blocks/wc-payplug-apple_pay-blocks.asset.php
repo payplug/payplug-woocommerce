34e0275430302c36b522ebc172a0911df7b88de2
Djalel <ghabri.djalel@gmail.com>
2024-11-25 10:35:18 +0000

    Apple Pay Cart build

--- assets/js/blocks/wc-payplug-apple_pay-blocks.asset.php
+++ assets/js/blocks/wc-payplug-apple_pay-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'd8ab31a8d3dc186846e7');
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '5155b7ad3f816e24888d');
--- assets/js/blocks/wc-payplug-apple_pay-blocks.js
+++ assets/js/blocks/wc-payplug-apple_pay-blocks.js
@@ -10,6 +10,10 @@
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   apple_pay_CancelOrder: () => (/* binding */ apple_pay_CancelOrder),
+/* harmony export */   apple_pay_Payment: () => (/* binding */ apple_pay_Payment),
+/* harmony export */   apple_pay_PlaceOrderWithDummyData: () => (/* binding */ apple_pay_PlaceOrderWithDummyData),
+/* harmony export */   apple_pay_UpdateOrder: () => (/* binding */ apple_pay_UpdateOrder),
 /* harmony export */   apple_pay_update_payment: () => (/* binding */ apple_pay_update_payment),
 /* harmony export */   getPayment: () => (/* binding */ getPayment)
 /* harmony export */ });
@@ -17,12 +21,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _woocommerce_settings__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_woocommerce_settings__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! jquery */ "jquery");
 /* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react */ "react");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
 
 
 const settings = (0,_woocommerce_settings__WEBPACK_IMPORTED_MODULE_0__.getSetting)('apple_pay_data', {});
-
 const getPayment = (props, order_id) => {
   const data = getPaymentData(props);
   return new Promise((resolve, reject) => {
@@ -57,6 +58,244 @@ const apple_pay_update_payment = data => {
     });
   });
 };
+const apple_pay_UpdateOrder = data => {
+  return new Promise((resolve, reject) => {
+    jquery__WEBPACK_IMPORTED_MODULE_1___default().ajax({
+      type: 'POST',
+      data: data,
+      url: settings.ajax_url_update_applepay_order
+    }).success(function (response) {
+      resolve(response);
+    }).error(function (xhr, status, error) {
+      reject(error); // NOT WORKING!!
+    });
+  });
+};
+const apple_pay_Payment = data => {
+  return new Promise((resolve, reject) => {
+    jquery__WEBPACK_IMPORTED_MODULE_1___default().ajax({
+      type: 'POST',
+      data: data,
+      url: settings.ajax_url_update_applepay_payment
+    }).success(function (response) {
+      resolve(response);
+    }).error(function (xhr, status, error) {
+      reject(error); // NOT WORKING!!
+    });
+  });
+};
+const apple_pay_PlaceOrderWithDummyData = data => {
+  return new Promise((resolve, reject) => {
+    jquery__WEBPACK_IMPORTED_MODULE_1___default().ajax({
+      type: 'POST',
+      data: data,
+      url: settings.ajax_url_place_order_with_dummy_data
+    }).success(function (response) {
+      resolve(response);
+    }).error(function (xhr, status, error) {
+      reject(error); // NOT WORKING!!
+    });
+  });
+};
+const apple_pay_CancelOrder = data => {
+  return new Promise((resolve, reject) => {
+    jquery__WEBPACK_IMPORTED_MODULE_1___default().ajax({
+      type: 'POST',
+      data: data,
+      url: settings.ajax_url_applepay_cancel_order
+    }).success(function (response) {
+      resolve(response);
+    }).error(function (xhr, status, error) {
+      reject(error); // NOT WORKING!!
+    });
+  });
+};
+
+/***/ }),
+
+/***/ "./resources/js/frontend/wc-payplug-apple_pay_cart-blocks.js":
+/*!*******************************************************************!*\
+  !*** ./resources/js/frontend/wc-payplug-apple_pay_cart-blocks.js ***!
+  \*******************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @wordpress/element */ "@wordpress/element");
+/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _woocommerce_settings__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @woocommerce/settings */ "@woocommerce/settings");
+/* harmony import */ var _woocommerce_settings__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_woocommerce_settings__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _helper_wc_payplug_apple_pay_requests__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./helper/wc-payplug-apple_pay-requests */ "./resources/js/frontend/helper/wc-payplug-apple_pay-requests.js");
+
+
+
+
+const settings = (0,_woocommerce_settings__WEBPACK_IMPORTED_MODULE_2__.getSetting)('apple_pay_data', {});
+const ApplePayCart = props => {
+  let session = null;
+  let apple_pay_Session_status = null;
+  const apple_pay_btn = jQuery('apple-pay-button');
+  const apple_pay_wrapper = jQuery("#apple-pay-button-wrapper");
+  const apple_pay = {
+    load_order_total: false,
+    OrderPaymentCreated: function (response) {
+      if ('success' !== response.payment_data.result) {
+        apple_pay.CancelOrder();
+      }
+      apple_pay.BeginSession(response);
+    },
+    CreateSession: function () {
+      const request = {
+        "countryCode": settings.countryCode,
+        "currencyCode": settings.currencyCode,
+        "merchantCapabilities": ["supports3DS"],
+        "supportedNetworks": ["visa", "masterCard"],
+        "total": {
+          "label": "Apple Pay",
+          "type": "final",
+          "amount": props.billing.cartTotal.value / 100
+        },
+        'shippingMethods': settings.payplug_carriers,
+        'applicationData': btoa(JSON.stringify({
+          'apple_pay_domain': settings.apple_pay_domain
+        })),
+        'requiredBillingContactFields': ['postalAddress', 'name'],
+        'requiredShippingContactFields': ["postalAddress", "name", "phone", "email"]
+      };
+      session = new ApplePaySession(3, request);
+    },
+    CancelOrder: function () {
+      session.oncancel = event => {
+        (0,_helper_wc_payplug_apple_pay_requests__WEBPACK_IMPORTED_MODULE_3__.apple_pay_CancelOrder)({
+          'order_id': session.order_id,
+          'payment_id': session.payment_id
+        }).then(() => {
+          enabled_button();
+        });
+      };
+    },
+    BeginSession: function (response) {
+      session.payment_id = response.payment_data.payment_id;
+      session.order_id = response.order_id;
+      session.cancel_url = response.payment_data.cancel_url;
+      session.return_url = response.payment_data.return_url;
+      apple_pay.MerchantValidated(session, response.payment_data.merchant_session);
+      session.amount = parseFloat(settings.total / 100) * 100;
+      session.onshippingmethodselected = event => {
+        const shippingMethod = event.shippingMethod;
+        session.shippingMethod = shippingMethod.identifier;
+        const baseTotal = settings.total / 100;
+        let currentShippingCost = shippingMethod.amount;
+        const newTotalAmount = parseFloat(baseTotal) + parseFloat(currentShippingCost);
+        session.amount = newTotalAmount * 100;
+        const update = {
+          newTotal: {
+            label: 'Total',
+            amount: newTotalAmount
+          },
+          newLineItems: [{
+            label: shippingMethod.label,
+            type: 'final',
+            amount: currentShippingCost
+          }]
+        };
+        session.completeShippingMethodSelection(update);
+      };
+      session.begin();
+    },
+    MerchantValidated: function (session, merchant_session) {
+      session.onvalidatemerchant = event => {
+        try {
+          session.completeMerchantValidation(merchant_session);
+        } catch (err) {
+          apple_pay.CancelOrder();
+        }
+      };
+    },
+    AddErrorMessage: function (message) {
+      apple_pay_wrapper.append(jQuery('<div class="apple-pay-cart-notice"></div>').append("<span>" + message + "</span>"));
+    },
+    DeleteErrorMessage: function () {
+      setTimeout(function () {
+        jQuery('.apple-pay-cart-notice').contents().first().remove();
+      }, 4000);
+    }
+  };
+  function CheckPaymentOnPaymentAuthorized() {
+    return new Promise((resolve, reject) => {
+      session.onpaymentauthorized = event => {
+        let event_data = event.payment;
+        let data = {
+          'order_id': session.order_id,
+          'shipping': event_data.shippingContact,
+          'billing': event_data.billingContact,
+          'shipping_method': session.shippingMethod
+        };
+        (0,_helper_wc_payplug_apple_pay_requests__WEBPACK_IMPORTED_MODULE_3__.apple_pay_UpdateOrder)(data).then(result_order => {
+          data = {
+            'action': 'applepay_update_payment',
+            'post_type': 'POST',
+            'payment_id': session.payment_id,
+            'payment_token': event.payment.token,
+            'order_id': session.order_id,
+            'amount': session.amount
+          };
+          (0,_helper_wc_payplug_apple_pay_requests__WEBPACK_IMPORTED_MODULE_3__.apple_pay_Payment)(data).then(result_payment => {
+            apple_pay_Session_status = ApplePaySession.STATUS_SUCCESS;
+            if (result_payment.success !== true) {
+              apple_pay_Session_status = ApplePaySession.STATUS_FAILURE;
+              apple_pay.AddErrorMessage(result_payment.data.message);
+              apple_pay.DeleteErrorMessage();
+              apple_pay.CancelOrder();
+            }
+            session.completePayment({
+              "status": apple_pay_Session_status
+            });
+            resolve();
+          });
+        });
+      };
+    });
+  }
+  jQuery('apple-pay-button').on("click", e => {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    disabled_button();
+    apple_pay.CreateSession();
+    apple_pay.CancelOrder();
+    (0,_helper_wc_payplug_apple_pay_requests__WEBPACK_IMPORTED_MODULE_3__.apple_pay_PlaceOrderWithDummyData)().then(async response => {
+      if (response.success === false) {
+        apple_pay.AddErrorMessage(response.data.message);
+        apple_pay.DeleteErrorMessage();
+        enabled_button();
+        return;
+      }
+      settings.total = response.total;
+      apple_pay.OrderPaymentCreated(response);
+      await CheckPaymentOnPaymentAuthorized().then(res => {
+        window.location = session.return_url;
+      });
+    });
+  });
+  function disabled_button() {
+    apple_pay_btn.addClass("isDisabled");
+  }
+  function enabled_button() {
+    apple_pay_btn.removeClass("isDisabled");
+  }
+  return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("div", {
+    id: "apple-pay-button-wrapper"
+  }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("apple-pay-button", {
+    buttonstyle: "black",
+    type: "pay",
+    locale: settings?.payplug_locale
+  })));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ApplePayCart);
 
 /***/ }),
 
@@ -228,6 +467,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! react */ "react");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_6__);
 /* harmony import */ var _helper_wc_payplug_apple_pay_requests__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./helper/wc-payplug-apple_pay-requests */ "./resources/js/frontend/helper/wc-payplug-apple_pay-requests.js");
+/* harmony import */ var _wc_payplug_apple_pay_cart_blocks__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./wc-payplug-apple_pay_cart-blocks */ "./resources/js/frontend/wc-payplug-apple_pay_cart-blocks.js");
+
 
 
 
@@ -403,6 +644,37 @@ const ApplePay = {
     features: settings.supports
   }
 };
+
+/**
+ *
+ * @param props
+ * @returns {JSX.Element}
+ * @Content for express payment method
+ */
+const ExpressContent = props => {
+  return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wc_payplug_apple_pay_cart_blocks__WEBPACK_IMPORTED_MODULE_8__["default"], props));
+};
+const ExpressApplePay = {
+  name: "apple_pay",
+  content: (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(ExpressContent, null),
+  edit: (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(ExpressContent, null),
+  canMakePayment: data => {
+    if (!settings?.is_cart) {
+      return false;
+    }
+    let payplug_authorized_carriers = settings.payplug_authorized_carriers;
+    let selected_shipping = data.selectedShippingMethods[0].split(":");
+    let authorized = false;
+    payplug_authorized_carriers.forEach(function (item) {
+      if (selected_shipping[0] === item) {
+        authorized = true;
+      }
+    });
+    return authorized;
+  },
+  paymentMethodId: "apple_pay"
+};
+(0,_woocommerce_blocks_registry__WEBPACK_IMPORTED_MODULE_5__.registerExpressPaymentMethod)(ExpressApplePay);
 (0,_woocommerce_blocks_registry__WEBPACK_IMPORTED_MODULE_5__.registerPaymentMethod)(ApplePay);
 /******/ })()
 ;
