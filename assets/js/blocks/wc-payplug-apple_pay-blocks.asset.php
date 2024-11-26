73874e96b42ac50264883f177a621ad801233c72
Manuel Mesquita <mmesquita-ext@payplug.com>
2024-11-26 23:31:16 +0700

    WOOC-1533

--- assets/js/blocks/wc-payplug-apple_pay-blocks.asset.php
+++ assets/js/blocks/wc-payplug-apple_pay-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '5155b7ad3f816e24888d');
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '0a6a2afdb6ddce287441');
