90c2a2abdc84f2f5725e9056c69f2e6c50e72e52
Djalel <ghabri.djalel@gmail.com>
2024-11-29 10:27:24 +0000

    Building blocks

--- assets/js/blocks/wc-payplug-apple_pay-blocks.asset.php
+++ assets/js/blocks/wc-payplug-apple_pay-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '0a6a2afdb6ddce287441');
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'f9b53e717e7f38c544ac');
