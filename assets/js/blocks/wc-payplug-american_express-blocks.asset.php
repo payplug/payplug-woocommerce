dbb0fdbdc1dc257710a4853c3087c71fa456c363
Djalel <ghabri.djalel@gmail.com>
2024-12-22 11:53:53 +0000

    WOOC-1467 : Improvements

--- assets/js/blocks/wc-payplug-american_express-blocks.asset.php
+++ assets/js/blocks/wc-payplug-american_express-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'ba9276b8fcbf5b01d14e');
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '48400d7635507f791bec');
