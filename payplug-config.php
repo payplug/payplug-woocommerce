5b2b849476ff50bbd0a5b907b4bf6c1d3115a936
Manuel Mesquita <mmesquita-ext@payplug.com>
2025-02-17 11:29:40 +0000

    WOOC-1608

---
+++ payplug-config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The base configuration for payplug payment gateways
+ *
+ * This file contains the following configurations:
+ * * required CDN path
+ * @package payplug
+ */
+
+/**
+ *  CDN path to integrated-payment lib
+ */
+define( 'IP_API', 'https://cdn-qa.payplug.com/js/integrated-payment/v1@1/index.js' );
