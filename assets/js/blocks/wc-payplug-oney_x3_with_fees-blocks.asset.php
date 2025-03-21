2bc2ef1dd1bb848b1fb8841e86fe3dbd143afe0e
Manuel Mesquita <mmesquita-ext@payplug.com>
2025-03-21 19:36:52 +0000

    rebuild blocks

--- assets/js/blocks/wc-payplug-oney_x3_with_fees-blocks.asset.php
+++ assets/js/blocks/wc-payplug-oney_x3_with_fees-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '103bdad21905bde1f4e1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '92a313570610ce7d3b72');
