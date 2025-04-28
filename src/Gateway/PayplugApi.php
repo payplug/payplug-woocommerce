f10ebb5cc286fbfe00f4889ea1f2e07eae6bede7
Hadrien De Laforce <hdelaforce@payplug.com>
2025-04-28 14:19:32 +0200

    PRE-2843 refactor: Update AdminUI for unify authentication

--- src/Gateway/PayplugApi.php
+++ src/Gateway/PayplugApi.php
@@ -38,6 +38,19 @@ class PayplugApi {
 	}
 
 	/**
+	 * Get register url
+	 *
+	 * @param string $callback_uri
+	 *
+	 * @return object
+	 * @throws \Payplug\Exception\ConfigurationException
+	 */
+	public function retrieve_register_url($callback_uri)
+	{
+		return $this->do_request_with_fallback( '\Payplug\Authentication::getRegisterUrl', [$callback_uri, $callback_uri] );
+	}
+
+	/**
 	 * Configure PayPlug client.
 	 */
 	public function init() {
