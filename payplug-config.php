881a25a05d3291b8f8b2312be08181abe06474c3
Imène <ilajili@payplug.com>
2025-07-24 15:21:49 +0200

    PRE-2954 add config with IP +hf

--- payplug-config.php
+++ payplug-config.php
@@ -11,3 +11,9 @@
  *  CDN path to Hosted-Fields lib
  */
 define( 'HF_API', 'https://staging-internal-payment.gcp.dlns.io/ui/hosted-fields-lib/hosted-fields/v2.1.0/hosted-fields.min.js');
+/**
+ *  CDN path to integrated-payment lib
+ */
+define( 'IP_API', 'https://cdn-qa.payplug.com/js/integrated-payment/v1@1/index.js' );
+define('USE_HOSTED_FIELDS', true);
+define('INTEGRATED_PAYMENT', true);
