5bdea9f1bfe37a4c82408207c79338c3edadf6e6
Imène <ilajili@payplug.com>
2026-04-23 16:00:11 +0200

    PRE-3329 feat: create a paayment link with MCP wooco

--- assets/js/blocks/wc-payplug-wero-blocks.asset.php
+++ assets/js/blocks/wc-payplug-wero-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'ac04d0d454f612028740');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '4d6e9715736c6c68ab6a');
